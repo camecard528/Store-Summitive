3d7e20fdb26b7d87a95466010f2bdcce6847fa28
temp <temp>
2016-10-14 13:08:11 -0400

    Add project files.

---
+++ Store Summitive/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Store_Summitive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tacoLabel = new System.Windows.Forms.Label();
+            this.lambLabel = new System.Windows.Forms.Label();
+            this.turkeyLabel = new System.Windows.Forms.Label();
+            this.menuLabel = new System.Windows.Forms.Label();
+            this.milkLabel = new System.Windows.Forms.Label();
+            this.popLabel = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tacoText = new System.Windows.Forms.TextBox();
+            this.lambText = new System.Windows.Forms.TextBox();
+            this.turkeyText = new System.Windows.Forms.TextBox();
+            this.milkText = new System.Windows.Forms.TextBox();
+            this.popText = new System.Windows.Forms.TextBox();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.subLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.tenderedText = new System.Windows.Forms.TextBox();
+            this.totalresLabel = new System.Windows.Forms.Label();
+            this.taxresLabel = new System.Windows.Forms.Label();
+            this.subresLabel = new System.Windows.Forms.Label();
+            this.changeresLabel = new System.Windows.Forms.Label();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tacoLabel
+            // 
+            this.tacoLabel.AutoSize = true;
+            this.tacoLabel.Location = new System.Drawing.Point(7, 35);
+            this.tacoLabel.Name = "tacoLabel";
+            this.tacoLabel.Size = new System.Drawing.Size(111, 13);
+            this.tacoLabel.TabIndex = 2;
+            this.tacoLabel.Text = "Curry Filled Beef Taco";
+            // 
+            // lambLabel
+            // 
+            this.lambLabel.AutoSize = true;
+            this.lambLabel.Location = new System.Drawing.Point(7, 57);
+            this.lambLabel.Name = "lambLabel";
+            this.lambLabel.Size = new System.Drawing.Size(85, 13);
+            this.lambLabel.TabIndex = 4;
+            this.lambLabel.Text = "Lamb Quesadilla";
+            // 
+            // turkeyLabel
+            // 
+            this.turkeyLabel.AutoSize = true;
+            this.turkeyLabel.Location = new System.Drawing.Point(7, 81);
+            this.turkeyLabel.Name = "turkeyLabel";
+            this.turkeyLabel.Size = new System.Drawing.Size(114, 13);
+            this.turkeyLabel.TabIndex = 6;
+            this.turkeyLabel.Text = "Jhatka Turkey Burgers";
+            // 
+            // menuLabel
+            // 
+            this.menuLabel.AutoSize = true;
+            this.menuLabel.Location = new System.Drawing.Point(12, 9);
+            this.menuLabel.Name = "menuLabel";
+            this.menuLabel.Size = new System.Drawing.Size(39, 13);
+            this.menuLabel.TabIndex = 7;
+            this.menuLabel.Text = "MENU";
+            // 
+            // milkLabel
+            // 
+            this.milkLabel.AutoSize = true;
+            this.milkLabel.Location = new System.Drawing.Point(7, 104);
+            this.milkLabel.Name = "milkLabel";
+            this.milkLabel.Size = new System.Drawing.Size(56, 13);
+            this.milkLabel.TabIndex = 9;
+            this.milkLabel.Text = "Goats milk";
+            // 
+            // popLabel
+            // 
+            this.popLabel.AutoSize = true;
+            this.popLabel.Location = new System.Drawing.Point(7, 130);
+            this.popLabel.Name = "popLabel";
+            this.popLabel.Size = new System.Drawing.Size(26, 13);
+            this.popLabel.TabIndex = 11;
+            this.popLabel.Text = "Pop";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(201, 104);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 13);
+            this.label13.TabIndex = 12;
+            // 
+            // tacoText
+            // 
+            this.tacoText.Location = new System.Drawing.Point(159, 35);
+            this.tacoText.Name = "tacoText";
+            this.tacoText.Size = new System.Drawing.Size(100, 20);
+            this.tacoText.TabIndex = 13;
+            // 
+            // lambText
+            // 
+            this.lambText.Location = new System.Drawing.Point(159, 57);
+            this.lambText.Name = "lambText";
+            this.lambText.Size = new System.Drawing.Size(100, 20);
+            this.lambText.TabIndex = 14;
+            // 
+            // turkeyText
+            // 
+            this.turkeyText.Location = new System.Drawing.Point(159, 81);
+            this.turkeyText.Name = "turkeyText";
+            this.turkeyText.Size = new System.Drawing.Size(100, 20);
+            this.turkeyText.TabIndex = 15;
+            // 
+            // milkText
+            // 
+            this.milkText.Location = new System.Drawing.Point(159, 104);
+            this.milkText.Name = "milkText";
+            this.milkText.Size = new System.Drawing.Size(100, 20);
+            this.milkText.TabIndex = 16;
+            // 
+            // popText
+            // 
+            this.popText.Location = new System.Drawing.Point(159, 130);
+            this.popText.Name = "popText";
+            this.popText.Size = new System.Drawing.Size(100, 20);
+            this.popText.TabIndex = 17;
+            // 
+            // totalButton
+            // 
+            this.totalButton.Location = new System.Drawing.Point(10, 166);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(249, 23);
+            this.totalButton.TabIndex = 18;
+            this.totalButton.Text = "Calculate Totals";
+            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
+            // 
+            // subLabel
+            // 
+            this.subLabel.AutoSize = true;
+            this.subLabel.Location = new System.Drawing.Point(10, 196);
+            this.subLabel.Name = "subLabel";
+            this.subLabel.Size = new System.Drawing.Size(46, 13);
+            this.subLabel.TabIndex = 19;
+            this.subLabel.Text = "Subtotal";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(12, 220);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(25, 13);
+            this.taxLabel.TabIndex = 20;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(12, 245);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 21;
+            this.totalLabel.Text = "Total";
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Location = new System.Drawing.Point(10, 276);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(53, 13);
+            this.tenderedLabel.TabIndex = 22;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(10, 299);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(249, 23);
+            this.changeButton.TabIndex = 23;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Location = new System.Drawing.Point(12, 331);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(44, 13);
+            this.changeLabel.TabIndex = 24;
+            this.changeLabel.Text = "Change";
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Location = new System.Drawing.Point(10, 359);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(249, 23);
+            this.receiptButton.TabIndex = 25;
+            this.receiptButton.Text = "PRINT RECEIPT";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // tenderedText
+            // 
+            this.tenderedText.Location = new System.Drawing.Point(159, 276);
+            this.tenderedText.Name = "tenderedText";
+            this.tenderedText.Size = new System.Drawing.Size(100, 20);
+            this.tenderedText.TabIndex = 26;
+            // 
+            // totalresLabel
+            // 
+            this.totalresLabel.AutoSize = true;
+            this.totalresLabel.Location = new System.Drawing.Point(156, 245);
+            this.totalresLabel.Name = "totalresLabel";
+            this.totalresLabel.Size = new System.Drawing.Size(41, 13);
+            this.totalresLabel.TabIndex = 27;
+            this.totalresLabel.Text = "label11";
+            // 
+            // taxresLabel
+            // 
+            this.taxresLabel.AutoSize = true;
+            this.taxresLabel.Location = new System.Drawing.Point(156, 220);
+            this.taxresLabel.Name = "taxresLabel";
+            this.taxresLabel.Size = new System.Drawing.Size(41, 13);
+            this.taxresLabel.TabIndex = 28;
+            this.taxresLabel.Text = "label14";
+            // 
+            // subresLabel
+            // 
+            this.subresLabel.AutoSize = true;
+            this.subresLabel.Location = new System.Drawing.Point(156, 196);
+            this.subresLabel.Name = "subresLabel";
+            this.subresLabel.Size = new System.Drawing.Size(41, 13);
+            this.subresLabel.TabIndex = 29;
+            this.subresLabel.Text = "label15";
+            // 
+            // changeresLabel
+            // 
+            this.changeresLabel.AutoSize = true;
+            this.changeresLabel.Location = new System.Drawing.Point(159, 331);
+            this.changeresLabel.Name = "changeresLabel";
+            this.changeresLabel.Size = new System.Drawing.Size(41, 13);
+            this.changeresLabel.TabIndex = 30;
+            this.changeresLabel.Text = "label16";
+            // 
+            // orderButton
+            // 
+            this.orderButton.Location = new System.Drawing.Point(356, 359);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(149, 23);
+            this.orderButton.TabIndex = 31;
+            this.orderButton.Text = "NEW ORDER";
+            this.orderButton.UseVisualStyleBackColor = true;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(551, 384);
+            this.Controls.Add(this.orderButton);
+            this.Controls.Add(this.changeresLabel);
+            this.Controls.Add(this.subresLabel);
+            this.Controls.Add(this.taxresLabel);
+            this.Controls.Add(this.totalresLabel);
+            this.Controls.Add(this.tenderedText);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subLabel);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.popText);
+            this.Controls.Add(this.milkText);
+            this.Controls.Add(this.turkeyText);
+            this.Controls.Add(this.lambText);
+            this.Controls.Add(this.tacoText);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.popLabel);
+            this.Controls.Add(this.milkLabel);
+            this.Controls.Add(this.menuLabel);
+            this.Controls.Add(this.turkeyLabel);
+            this.Controls.Add(this.lambLabel);
+            this.Controls.Add(this.tacoLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Indian Guys Burgers and Tacos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tacoLabel;
+        private System.Windows.Forms.Label lambLabel;
+        private System.Windows.Forms.Label turkeyLabel;
+        private System.Windows.Forms.Label menuLabel;
+        private System.Windows.Forms.Label milkLabel;
+        private System.Windows.Forms.Label popLabel;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tacoText;
+        private System.Windows.Forms.TextBox lambText;
+        private System.Windows.Forms.TextBox turkeyText;
+        private System.Windows.Forms.TextBox milkText;
+        private System.Windows.Forms.TextBox popText;
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Label subLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.TextBox tenderedText;
+        private System.Windows.Forms.Label totalresLabel;
+        private System.Windows.Forms.Label taxresLabel;
+        private System.Windows.Forms.Label subresLabel;
+        private System.Windows.Forms.Label changeresLabel;
+        private System.Windows.Forms.Button orderButton;
+    }
+}
+
---
+++ Store Summitive/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Store_Summitive
+{
+    public partial class Form1 : Form
+    {   const double TACO = 2.99;
+        const double QUESADILLA= 5.99;
+        const double BURGER = 4.99;
+        const double MILK = 2.50;
+        const double POP = 0.99;
+        const double TAX = 0.13;
+        int taco;
+        int quesadilla;
+        int burger;
+        int milk;
+        int pop;
+        double tendered;
+        double totalPrice;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+            Graphics formgraphics = this.CreateGraphics();
+            SolidBrush textBrush = new SolidBrush(Color.Black);
+            Font receiptFont = new Font("Arial", 7, FontStyle.Bold);
+
+            try
+            {
+                taco = Convert.ToInt32(tacoText.Text);
+                quesadilla = Convert.ToInt32(lambText.Text);
+                burger = Convert.ToInt32(turkeyText.Text);
+                milk = Convert.ToInt32(milkText.Text);
+                pop = Convert.ToInt32(popText.Text);
+
+
+                //calculate cost 
+                double subtotalPrice = taco * TACO + quesadilla * QUESADILLA + burger * BURGER
+                    + milk * MILK + pop * POP;
+                double totalTax = subtotalPrice * TAX;
+                totalPrice = subtotalPrice + totalTax;
+
+                // print prices
+                subresLabel.Text = subtotalPrice.ToString("C");
+                totalresLabel.Text = totalPrice.ToString("C");
+                taxresLabel.Text = totalTax.ToString("C");
+            }
+
+            catch
+            {
+                formgraphics.DrawString("use numbers only and enter 0 if customer doesn't order",
+                    receiptFont, textBrush, 305, 15);
+            }
+            
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            // change
+            tendered = Convert.ToDouble(tenderedText.Text);
+            double change = tendered - totalPrice;
+            changeresLabel.Text = change.ToString("C");
+
+
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer soundregister = new SoundPlayer(Properties.Resources.register);
+            
+
+            taco = Convert.ToInt32(tacoText.Text);
+            quesadilla = Convert.ToInt32(lambText.Text);
+            burger = Convert.ToInt32(turkeyText.Text);
+            milk = Convert.ToInt32(milkText.Text);
+            pop = Convert.ToInt32(popText.Text);
+
+            Graphics formgraphics = this.CreateGraphics();
+            SolidBrush receiptBrush = new SolidBrush(Color.White);
+            SolidBrush textBrush = new SolidBrush(Color.Black);
+            Font receiptFont = new Font("Arial", 7, FontStyle.Bold);
+            formgraphics.FillRectangle(receiptBrush, 295, 10, 240, 365);
+
+            formgraphics.DrawString("INDIAN GUYS BURGERS AND TACOS CO-OP", receiptFont,textBrush ,305, 15);
+            formgraphics.DrawString("Tacos x" + taco, receiptFont, textBrush, 305, 25);
+            formgraphics.DrawString("Quesadillas x"+  quesadilla, receiptFont, textBrush, 305, 35);
+            formgraphics.DrawString("Burgers x" + burger, receiptFont, textBrush, 305, 45);
+            formgraphics.DrawString("Milk x"+ milk, receiptFont, textBrush, 305, 55);
+            formgraphics.DrawString("Pop x"+ pop, receiptFont, textBrush, 305, 65);
+            soundregister.Play();
+
+
+
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {
+            subresLabel.Text = "";
+            totalresLabel.Text = "";
+            taxresLabel.Text = "";
+            changeresLabel.Text = "";
+
+
+        }
+    }
+}
